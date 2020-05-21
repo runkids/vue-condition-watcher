9176cd6f04e726692b9b3a211e1af7fba29684f0
Willy Hong <egg358358@gmail.com>
2020-05-21 17:01:32 +0800

    chore: add test

---
+++ test/use-condition-watcher.test.ts
@@ -0,0 +1,52 @@
+import {useConditionWatcher} from '../src/index'
+import {isRef, isReactive} from 'vue'
+
+describe('useConditionWatcher', () => {
+  const consoleWarnSpy = jest.spyOn(console, "warn");
+
+  beforeEach(() => {
+    consoleWarnSpy.mockClear();
+  });
+
+  it(`Check return value's type`, async ()=> {
+    const config = {
+      fetcher: params => new Promise(resolve => resolve(params)),
+      conditions: {
+        gender: ['male'],
+        results: 9
+      }
+    }
+    const {conditions, data, error, loading, refresh} = useConditionWatcher(config)
+
+    expect(isReactive(conditions)).toBeTruthy()
+    expect(isRef(data)).toBeTruthy()
+    expect(isRef(error)).toBeTruthy()
+    expect(isRef(loading)).toBeTruthy()
+    expect(isRef(refresh)).toBeTruthy()
+    expect(typeof loading.value === 'boolean').toBeTruthy()
+    expect(typeof refresh.value === 'function').toBeTruthy()
+  })
+
+  it(`Check condition has been change`,()=>{
+    const config = {
+      fetcher: params => new Promise(resolve => resolve(params)),
+      conditions: {
+        gender: ['male'],
+        results: 9
+      }
+    }
+    const {conditions} = useConditionWatcher(config)
+
+    expect(conditions).toMatchObject({
+      gender: ['male'],
+      results: 9
+    })
+    conditions.results = 10
+    conditions.gender = ['male', 'female']
+
+    expect(conditions).toMatchObject({
+      gender: ['male', 'female'],
+      results: 10
+    })
+  })
+})
