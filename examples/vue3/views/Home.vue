8fd560bfad21c24e4e1f5f1a34d1eef63e4bc276
Willy Hong <egg358358@gmail.com>
2022-01-01 23:56:16 +0800

    release: release v1.1.2

--- examples/vue3/views/Home.vue
+++ examples/vue3/views/Home.vue
@@ -7,6 +7,7 @@
     <input class="date-picker" type="date" v-model="conditions.date" :disabled="loading">
     <button class="btn" @click="execute">Refetch</button>
     <button class="btn" @click="resetConditions">Reset</button>
+    <button class="btn" @click="conditions.offset += conditions.limit">Add Offset</button>
   </div>
 
   <div class="container" v-if="!loading">
@@ -23,13 +24,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useConditionWatcher } from '../../../src/index'
 // import { useConditionWatcher } from 'vue-condition-watcher'
 import api from '../api'
 
 export default defineComponent({
   setup(){
+    const cancelTrigger = ref(false)
     const { conditions, loading, data, execute, resetConditions, onConditionsChange } = useConditionWatcher(
       {
         fetcher: api.users,
@@ -44,6 +46,13 @@ export default defineComponent({
         defaultParams: {
           results: 9,
         },
+        beforeFetch(cond, cancel) {
+          if (cancelTrigger.value) {
+            cancel()
+            cancelTrigger.value = false
+          }
+          return cond
+        }
       }, 
       { 
         sync: 'router', 
@@ -53,8 +62,10 @@ export default defineComponent({
     )
 
     onConditionsChange(([newConditions, oldConditions])=> {
-      console.log('newConditions=',newConditions)
-      console.log('oldConditions=',oldConditions)
+      if (newConditions.offset !== 0 && newConditions.offset === oldConditions.offset) {
+        cancelTrigger.value = true
+        conditions.offset = 0
+      }
     })
 
     return {
